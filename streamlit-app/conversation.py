ddac0c7f4fa1fe7d099dfdf23bbe9b925e10f458
Guillermo <guillermo.rodriguez@snoopconsulting.com>
2024-06-04 16:08:05 -0300

    add streamlit app

---
+++ streamlit-app/conversation.py
@@ -0,0 +1,31 @@
+SHOW_LIST = 2
+SHOW_IPHONE = 3
+SHOW_SAMSUNG = 5
+SHOW_IPHONE_AGAIN = 8
+
+event_mapping = {
+    SHOW_LIST: ("plp", -1),
+    SHOW_SAMSUNG: ("pdp", 1),
+    SHOW_IPHONE: ("pdp", 2),
+    SHOW_IPHONE_AGAIN: ("pdp", 2),
+}
+
+
+conversation = [
+    "Hola, soy el chatbot, en que te puedo ayudar?",
+    "A la izquierda podes ver un listado de los mejores celulares",
+    "Hola! Estas hablando con el Samsung s24+",
+    "Hola! Soy el nuevo Iphone 15, preguntame lo que quieras saber sobre mí",
+]
+
+conversation_hector = [
+    "¡Qué bien!. Cuantos años tiene tu hijo?",
+    "En que gama estas buscando?",
+    "Muy bien. Teniendo en cuenta que es para tu hijo que va a cumplir 15 años y que lo usará para divertirse, te voy a mostrar unos móviles que puedes tener en cuenta. Te interesa alguno de ellos?",
+    "¡Hola, soy el iPhone 14 PRO!. Cuento con caracteristicas que me hacen muy bueno para jugar como mi gran pantalla y mi excelente nitidez de sonido	Mostrar iPhone 14 PRO. ¿Quieres saber algo más sobre mí?",
+    "Cuento con una pantalla de cristal de zafiro que es muy resistente a los golpes. Algunos compradores en este sitio dicen que he aguantado fuertes caidas sin romperme. ¿Hay algo más que quieras saber?",
+    "Te refieres al Samsung Galaxy S23 que te estoy mostrando?",
+    "Hola soy el Samsung Galaxy S23. Me llevan mucho para jugar ya que cuento con sufiente espacio para descargar juegos y mi pantalla de Amoled de xx pulgadas se ve muy bien. ¿Quieres saber algo más de mí?",
+    "Puedes usar la función find my mobile para localizar y en el peor de los casos borrar los datos los datos. Puedes encontrarme entrando a https://findmymobile.samsung.com/. ¿Algo más que quieras saber de mí?",
+    "De acuerdo, aquí te estoy mostrando nuevamente el iPhone 14 Pro"
+]
---
+++ streamlit-app/streamlit_app.py
@@ -0,0 +1,83 @@
+import time
+import random
+import requests
+import streamlit as st
+import streamlit.components.v1 as components
+from conversation import conversation, conversation_hector, event_mapping
+
+
+LLM_ENDPOINT = "http://127.0.0.1:8000/base"
+conversation_length = len(conversation_hector)
+conversation_steps = conversation_hector
+
+custom_component = components.declare_component(
+    "mycomponent",
+    path="./streamlit-app"
+)
+
+with open("./iframe/styles.css", "r") as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def generate_response(message):
+    data = {
+        "id": "streamlit-app-1",
+        "message": message
+    }
+
+    try:
+        response = requests.post(LLM_ENDPOINT, json=data, headers={"Content-Type": "application/json"})
+        response.raise_for_status()
+        assistant_response = response.json()
+    except requests.exceptions.RequestException as e:
+        assistant_response = "Hubo un error al obtener respuesta del bot."
+
+    return assistant_response
+
+def get_response(turn):
+    return conversation_steps[turn]
+
+def run_app():
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("Escriba aqui..."):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+
+            #assistant_response = generate_response(prompt)
+            assistant_response = get_response(st.session_state.turn)
+            for letra in assistant_response:
+                full_response += letra
+                time.sleep(0.02)
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+
+            event = event_mapping.get(int(st.session_state.turn))
+            if event:
+                print(event)
+                custom_component(action=event[0], id_product=event[1])
+        
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+        st.session_state.turn = (st.session_state.turn + 1) % conversation_length
+
+if __name__ == '__main__':
+    
+    st.title('Copilot de Compra')
+    if "turn" not in st.session_state:
+        st.session_state.turn = 0
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    run_app()
